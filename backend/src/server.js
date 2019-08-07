fabf6a6f72d2f24d58fd0cf83bd7cb8a5d4facfe
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-08-06 23:38:59 -0300

    Add connection database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const server = express();
 
-server.get('/', (req, res) => {
-    return res.send('Hello World');
+mongoose.connect('mongodb+srv://redfox:redfox@cluster-kjvqg.mongodb.net/omnistack?retryWrites=true&w=majority', {
+    useNewUrlParser: true
 });
+
+server.use(express.json());
+server.use(routes);
 
 server.listen(3333);
