e7aa2d0cb8e391deff9a54b9d2f717662b3e5fbe
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-08-07 23:04:11 -0300

    Add likes n' dislikes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const DevController = require('./Controllers/DevController');
+const LikeController = require('./Controllers/LikeController');
+const DislikeController = require('./Controllers/DislikeController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.send('Hello World');
-});
-
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const server = express();
 
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://redfox:redfox@cluster-kjvqg.mongodb.net/omnistac
   useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
